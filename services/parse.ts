1aba8073433bc980018b6306c6fb2972ec35980c
mrchn <mrnfll@icloud.com>
2026-06-27 19:24:27 +0300

    tighten regex in docx parser to prevent unclosed brace matching

--- services/parse.ts
+++ services/parse.ts
@@ -8,18 +8,17 @@ export const Parse = async (uri: string): Promise<string[]> => {
 		const zip = await JSZip.loadAsync(
 			await FileSystem.readAsStringAsync(
 				uri, { encoding: 'base64' }
-				), { base64: true }
-			);
-		const xml = await zip.file('word/document.xml')
-			?.async('text');
+			), { base64: true }
+		);
+		const xml = await zip.file('word/document.xml')?.async('text');
 		if (!xml) return [];
 		return [...new Set(
 			(
 				xml.replace(/<[^>]+>/g, '')
-					.match(/\{\{(.+?)\}\}/g) || []
+					.match(/\{\{([^\{\}]+?)\}\}/g) || []
 			)
 				.map(m => m.replace(/[\{\}\s\xa0]/g, ''))
 				.filter(Boolean)
 		)]
 	} catch { return [] }
-};
+}
