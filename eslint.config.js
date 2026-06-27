c3ad7848d1391b3242746dba1574c71c1cd9d81f
mrchn <mrnfll@icloud.com>
2026-06-27 19:31:03 +0300

    migrate to native flat config array and fix nested structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,6 @@
-const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-module.exports = defineConfig(
-	[
-		expoConfig,
-		{ ignores: ['dist/*'] }
-	]
-)
+module.exports = [...expoConfig, { ignores: [
+	'dist/*', '.expo/*', 'node_modules/*',
+	'ios/*', 'android/*'
+]}]
