39bae5e7d23c2d1a73dbbe575582347c61864356
mrchn <mrnfll@icloud.com>
2026-06-27 16:33:20 +0300

    optimize code and safe-guard expo config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,17 +14,21 @@ import {
 
 export default function RootLayout() {
 
-	const theme = useColorScheme()==='dark'?DarkTheme:DefaultTheme
-
-	useEffect(() => { if (Platform.OS === 'ios') {
-		const version = Constants.expoConfig?.version ?? 'undefined';
-		const buildNumber = Constants.expoConfig?.ios.buildNumber ?? 'undefined';
-		Settings.set({app_version: `${version}`, app_build_number: `${buildNumber}`})
-	}}, []);
+	useEffect(() => {
+		const config = Constants.expoConfig;
+		Settings.set({
+			app_version: config?.version ?? 'undefined',
+			app_build_number: config?.ios?.buildNumber ?? 'undefined'
+		})
+	}, []);
 
 	return (
 		<GestureHandlerRootView style={{flex: 1}}>
-			<ThemeProvider value={theme}>
+			<ThemeProvider
+				value={
+					useColorScheme() === 'dark'?DarkTheme:DefaultTheme
+				}
+			>
 				<Stack>
 					<Stack.Screen
 						name='index' options={{headerShown:false}}/>
