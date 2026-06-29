ba8b194aa24fb82950d874f1d5b765a4cb68f40f
mrchn <mrnfll@icloud.com>
2026-06-30 02:45:48 +0300

    moved 'server' logic to Create

--- services/create.ts
+++ services/create.ts
@@ -1,13 +1,46 @@
 // @/services/create
 
+import JSZip from 'jszip'
 import * as FileSystem from 'expo-file-system/legacy'
 import * as Sharing from 'expo-sharing'
-import { Generate } from './server'
 import { convertPDF } from './convert'
 import type { CreateProps } from './interfaces'
 
+const fillTemplate = async (
+	temp: string, title: string, data: Record<string, string>
+) => {
+	try {
+		const zip = await JSZip.loadAsync(
+			await FileSystem.readAsStringAsync(
+				temp, { encoding: 'base64' }
+				), { base64: true }
+			)
+		let xml = await zip.file('word/document.xml')?.async('text')
+		if (!xml) return null
+		Object.entries(data).forEach(([key, value]) => {
+			const tag = '(?:<[^>]+>)*'
+			const esc_key = key.split('')
+				.map(char => char.replace(
+					/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'
+				)).join(tag)
+			xml = xml!.replace(new RegExp(
+				`\\{${tag}\\{${tag}\\s*${tag}` +
+				`${esc_key}` +
+				`\\s*${tag}\\}${tag}\\}`, 'g'
+			), () => value)
+		})
+		zip.file('word/document.xml', xml)
+		const uri = FileSystem.documentDirectory + `craft_${title}`
+		await FileSystem.writeAsStringAsync(
+			uri, await zip.generateAsync({ type: 'base64' }),
+			{ encoding: 'base64' }
+		)
+		return uri
+	} catch { return null }
+}
+
 export async function Create({ doc, data }: CreateProps) {
-	const uri = await Generate(doc.uri, doc.title, data)
+	const uri = await fillTemplate(doc.uri, doc.title, data)
 	if (!uri) return false
 	try {
 		const pdf_uri = `${FileSystem.cacheDirectory}craft.pdf`
