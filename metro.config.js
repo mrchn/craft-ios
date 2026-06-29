f0d0cc81520f2254c8b0dd3a8c38d4d082a1f283
mrchn <mrnfll@icloud.com>
2026-06-29 23:42:25 +0300

    changed import to default expo-/metro-config

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,3 @@
-const {getDefaultConfig} = require('@react-native/metro-config');
+const {getDefaultConfig} = require('@expo/metro-config');
 const config = getDefaultConfig(__dirname);
 module.exports = config
